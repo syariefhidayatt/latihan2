af461ed77599169ca990ebea3ffc082adb1ed484
syariefhidayatt <153275040+syariefhidayatt@users.noreply.github.com>
2024-10-07 15:38:20 +0700

    Delete UI - Hajj & Umroh directory

--- UI - Hajj & Umroh/scripts.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector(".fixed-top");
-window.onscroll = () => {
-  if (window.scrollY > 300) {
-    navbar.classList.add("scroll-nav-active");
-  } else {
-    navbar.classList.remove("scroll-nav-active");
-  }
-};
